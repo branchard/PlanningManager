9ef6085a2605adc77bf2a02a9eef996debecc1f7
Benoit <benoit.branchard@gmail.com>
2014-10-16 15:59:30 +0200

    warning user system on bad login/password

--- index.php
+++ index.php
@@ -3,6 +3,23 @@ define('ROOT_PATH', './');
 define('INCLUDES_PATH', './includes/'); 
 include INCLUDES_PATH.'header.php'; ?>
 	<section id="login-form-section">
+        <?php
+             if(isset($_GET['reason']))
+             {
+                echo '<div id="login_reason_info_wrapper"><p id="login_reason_info">';
+                switch ($_GET['reason']) {
+                    case 'login_missed':
+                        echo 'Vous devez rentrer un login et un password';
+                    break;
+                    case 'login_bad':
+                        echo 'Mauvais login ou password';
+                    break;
+                    default:
+                        echo 'Erreur inconue';
+                }
+                echo '</p></div>';
+             }
+        ?>
             <form id="main-login" method="post" action="login.php" class="login">
                 <p>
                     <label for="login">Username:</label>
