abe98fc1249fbf3d938c987492ca1d764dce6583
benoitbranchard <benoit.branchard@gmail.com>
2014-10-24 23:23:53 +0200

    more security

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,15 @@
 <?php
-// Connection au serveur
+// database.php
+//$database = array(
+//    'driver' => 'mysql',
+//    'host' => '***',
+//    'login' => '***',
+//    'password' => '***',
+//    'database' => '***',
+//);
+require 'database.php';
 try {
-    $dns = 'mysql:host=localhost;dbname=planning_manager';// à changer
-    $utilisateur = 'benoit';// à changer
-    $motDePasse = 'azerty';// à changer
-    $connection = new PDO($dns, $utilisateur, $motDePasse, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $connection = new PDO($database['driver'].':host='.$database['host'].';dbname='.$database['database'], $database['login'], $database['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     echo "Connection à MySQL impossible : ", $e->getMessage();
     die();
